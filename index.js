8d8f04d9f0ee85d6cb57ca4443931926c6e65a37
Villwulf <rcaguilera97@gmail.com>
2023-05-03 13:29:19 +0200

    remove package-lock, update index.js to run https http2, new dependencies

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Modulos
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const spdy = require("spdy");
+const fs = require("fs");
 const morgan = require("morgan");
 const port = 3001;
 const users = require("./routes/users");
@@ -8,16 +10,43 @@ const premios = require("./routes/premios");
 const horarios = require("./routes/horarios");
 const admin = require("./routes/admin");
 
+// .env
 require("dotenv").config();
 
+// Crear instancia de Express
+const app = express();
+
+// Configuración de middleware
 app.use(cors());
 app.use(morgan("tiny"));
 
+
+// Rutas app
 app.use("/api/users/", users);
 app.use("/api/premios/", premios);
 app.use("/api/horarios/", horarios);
 app.use("/api/admin/", admin);
 
+// Ruta de verificación
 app.get("/api", (_, res) => res.send("ZK Mansion API 1.0.0 Working!!"));
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+const httpsOptions = {
+  key: fs.existsSync('./certs/domain.pem') ? fs.readFileSync('./certs/domain.pem') : null,
+  cert: fs.existsSync('./certs/domain.pem') ? fs.readFileSync('./certs/domain.pem') : null
+}
+
+// Lanzamos el servicio con https (http2) con spdy en el puerto 3001
+// No usamos el paquete https porque usa http 1.1 (mas lento)
+// Si no se encuentran certificados se lanza el servidor http para trabajar en local
+if (httpsOptions.key && httpsOptions.cert) {
+  server = spdy.createServer(httpsOptions, app);
+  console.log(`App listening on HTTPS at port ${port}`);
+} else {
+  server = app;
+  console.log(`App listening on HTTP at port ${port}`);
+}
+
+// Levantamos el servicio según los criterios previos
+server.listen(port);
+
+// app.listen(port, () => console.log(`App listening on port ${port}`));
