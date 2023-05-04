88fbae95fd590aebeb89185fd2313c9a508755ae
Villwulf <rcaguilera97@gmail.com>
2023-05-04 11:58:21 +0200

    improve server up and add dev environment to run

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const spdy = require("spdy");
 const fs = require("fs");
 const morgan = require("morgan");
-const port = 3001;
 const users = require("./routes/users");
 const premios = require("./routes/premios");
 const horarios = require("./routes/horarios");
@@ -35,18 +34,28 @@ const httpsOptions = {
   cert: fs.existsSync('./certs/domain.pem') ? fs.readFileSync('./certs/domain.pem') : null
 }
 
-// Lanzamos el servicio con https (http2) con spdy en el puerto 3001
+// Lanzamos el servicio con https (http2) con spdy en el puerto 443
 // No usamos el paquete https porque usa http 1.1 (mas lento)
-// Si no se encuentran certificados se lanza el servidor http para trabajar en local
-if (httpsOptions.key && httpsOptions.cert) {
-  server = spdy.createServer(httpsOptions, app);
-  console.log(`App listening on HTTPS at port ${port}`);
+// Si no se encuentran certificados se lanza el servidor http en el puerto 80
+// Si el entorno no es prod se lanza en el puerto 3001 para development
+if (process.env.NODE_ENV == "prod") {
+  if (httpsOptions.key && httpsOptions.cert) {
+    server = spdy.createServer(httpsOptions, app);
+    port = 443;
+    service = "HTTPS";
+  } else {
+    server = app;
+    port = 80;
+    service = "HTTP";
+  }
 } else {
   server = app;
-  console.log(`App listening on HTTP at port ${port}`);
+  port = 3001;
+  service = "HTTP";
 }
 
 // Levantamos el servicio según los criterios previos
 server.listen(port);
+console.log(`App listening on ${service} at port ${port}`);
 
 // app.listen(port, () => console.log(`App listening on port ${port}`));
